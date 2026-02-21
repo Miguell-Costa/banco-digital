417cfbcfb97a94dc422b7a28c21a74c74363db47
Miguell-Costa <jmmc436@gmail.com>
2026-02-21 13:21:16 +0000

    add Transacao.java

---
+++ src/model/Transacao.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Transacao {
+    private TipoTransacao tipoTransacao;
+    private double valor;
+    private LocalDateTime dataHora;
+
+    // construtor
+    public Transacao(TipoTransacao tipoTransacao, double valor, LocalDateTime dataHora) {
+        this.tipoTransacao = tipoTransacao;
+        this.valor = valor;
+        this.dataHora = dataHora;
+    }
+
+    // getters e setters
+    public TipoTransacao getTipoTransacao() {
+        return tipoTransacao;
+    }
+
+    public void setTipoTransacao(TipoTransacao tipoTransacao) {
+        this.tipoTransacao = tipoTransacao;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Transacao{" +
+                "tipoTransacao=" + tipoTransacao +
+                ", valor=" + valor +
+                ", dataHora=" + dataHora +
+                '}';
+    }
+}
